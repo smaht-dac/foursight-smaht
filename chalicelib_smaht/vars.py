2c199b251810c8628d0e7972140c88e78ca52e5b
William Ronchetti <wrr33@cornell.edu>
2023-06-30 13:27:57 -0400

    allow HOST to be imported but empty, acquired from identity now

--- chalicelib_smaht/vars.py
+++ chalicelib_smaht/vars.py
@@ -3,4 +3,5 @@ from foursight_core.app_utils import AppUtilsCore
 
 FOURSIGHT_PREFIX = 'foursight-smaht'
 DEV_ENV = 'staging'
+HOST = ''  # should not be used
 CHECK_SETUP_FILE = join(dirname(__file__), AppUtilsCore.CHECK_SETUP_FILE_NAME)
