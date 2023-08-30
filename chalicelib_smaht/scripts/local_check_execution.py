af175b117a691711c60cf8e47e5b83b932e75a65
David Michaels <david_michaels@hms.harvard.edu>
2023-08-30 07:00:17 -0400

    Misc updates related to foursight-smaht.

---
+++ chalicelib_smaht/scripts/local_check_execution.py
@@ -0,0 +1,10 @@
+from foursight_core.scripts.local_check_execution import local_check_execution
+from chalicelib_smaht.app_utils import app_utils_obj as app_utils
+
+
+def main():
+    local_check_execution(app_utils)
+
+
+if __name__ == "__main__":
+    main()
